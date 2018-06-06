1fb3963c1bf23addb97cf344139024fa946fcc63
ChaosFlurry <chaosflurry@gmail.com>
2018-06-06 01:36:45 -0700

    Weather now updates every 5 minutes

--- src/Weather.java
+++ src/Weather.java
@@ -1,22 +1,35 @@
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Weather {
-    public static void main(String[] args) {
-        URL city = null;
-        try {
-            city = new URL("https://weather.gc.ca/rss/city/bc-74_e.xml");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+    // TODO improve this
+    private static WeatherReport weatherReport = null;
+    private static WeatherForecast weatherForecast = null;
+    
+    public static void main(String[] args) throws MalformedURLException {
+        URL weatherFeed = new URL("https://weather.gc.ca/rss/city/bc-74_e.xml");
         
         WeatherReportRetriever weatherReportRetriever = new WeatherReportRetriever();
         WeatherForecastRetriever weatherForecastRetriever = new WeatherForecastRetriever();
         
-        WeatherReport weatherReport = weatherReportRetriever.getWeatherReport(city);
-        WeatherForecast weatherForecast = weatherForecastRetriever.getWeatherForecast(city);
+        Runnable updateWeather = () -> {
+            if (weatherReport == null || !weatherReportRetriever.getWeatherReport(weatherFeed).getTime().equals(weatherReport.getTime())) {
+                weatherReport = weatherReportRetriever.getWeatherReport(weatherFeed);
+                System.out.println(weatherReport);
+            }
+            
+            if (weatherForecast == null || !weatherForecastRetriever.getWeatherForecast(weatherFeed).getIssuedTime().get(0).equals(weatherForecast.getIssuedTime().get(0))) {
+                weatherForecast = weatherForecastRetriever.getWeatherForecast(weatherFeed);
+                System.out.println(weatherForecast);
+            }
+        };
         
-        System.out.println(weatherReport);
-        System.out.println(weatherForecast);
+        // Updates the weather every 5 minutes only if there has been an update
+        ScheduledExecutorService weatherService = Executors.newSingleThreadScheduledExecutor();
+        weatherService.scheduleAtFixedRate(updateWeather, 0, 300, TimeUnit.SECONDS);
+        
     }
 }
